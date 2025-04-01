549f5f8de7454d8b4765091aa7049ae04e15b0c6
lEmAn-wq <leman2772020@gmail.com>
2025-04-02 02:17:03 +0700

    đổi hiệu ứng

--- lib/shared/custom_page_route.dart
+++ lib/shared/custom_page_route.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 class CustomPageRoute extends PageRouteBuilder {
   final Widget child;
+
   CustomPageRoute({required this.child})
     : super(
-        // pageBuilder: (context, animation, secondaryAnimation) => child,
         pageBuilder: (context, animation, secondaryAnimation) => child,
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        transitionsBuilder: (
+          context,
+          animation,
+          secondaryAnimation,
+          childTransition,
+        ) {
+          // hiệu ứng trượt từ phải sang trái
           const begin = Offset(1.0, 0.0);
           const end = Offset.zero;
           const curve = Curves.ease;
@@ -14,8 +20,12 @@ class CustomPageRoute extends PageRouteBuilder {
             begin: begin,
             end: end,
           ).chain(CurveTween(curve: curve));
-
+          // return SlideTransition(
+          //   position: animation.drive(tween),
+          //   child: childTransition,
+          // );
           return FadeTransition(opacity: animation, child: child);
         },
+        transitionDuration: const Duration(milliseconds: 5000),
       );
 }
